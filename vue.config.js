f1d06a3e14136733419d7a6ad01a9ec821817596
Nancy M Schorr <nms@schorrmedia.com>
2020-09-25 07:25:14 +0200

    standard commit

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,21 @@
 
 const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
+
+const fs = require('fs')
+const https = require('https')
+
 module.exports = {
-  publicPath: './',
-  outputDir: './dist',
-  transpileDependencies: [
-    'vuetify'
-  ],
-  runtimeCompiler: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/spc/' : '/',
+  outputDir: 'dist/',
+  devServer: {
+    https: {
+      cert: fs.readFileSync("/etc/letsencrypt/archive/westteam.nulstar.com/fullchain1.pem"),
+      key: fs.readFileSync("/etc/letsencrypt/archive/westteam.nulstar.com/privkey1.pem"),
+      rejectUnauthorized: false
+    },
+  public: 'https://localhost:5004/',
+  },
+  transpileDependencies: ['vuetify'],
   chainWebpack: config => {
     plugins: [
       new GoogleFontsPlugin({
@@ -19,4 +28,13 @@ module.exports = {
       })
     ]
   }
- }
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+    },
+  },
+}
+
